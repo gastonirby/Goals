f599465a8250df1d85fe6047f7945fa178888e2b
Gaston Irby III <gaston@Gastons-iMac.hsd1.wa.comcast.net>
2022-10-17 13:28:18 -0700

    styled delete

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,12 +1,17 @@
+import reactDom from 'react-dom';
 import { StyleSheet, View, Text, Pressable } from 'react-native';
 
 function GoalItem(props) {
     return (
-        <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-            <View style={styles.goalItem}>
+        <View style={styles.goalItem}>
+            <Pressable 
+                android_ripple={{ color: 'red' }} 
+                onPress={props.onDeleteItem.bind(this, props.id)}
+                style={({pressed}) => pressed && styles.pressedItem}
+            >
                 <Text style ={styles.goalText}>{props.text}</Text>
-            </View>
-        </Pressable>
+            </Pressable>
+        </View>
     );
 };
 
@@ -15,11 +20,16 @@ export default GoalItem;
 const styles = StyleSheet.create({
     goalItem: {
         margin: 8,
-        padding: 8,
         borderRadius: 6,
         backgroundColor: '#5e0acc',
       },
+    pressedItem: {
+        // opacity: 0.5,
+        borderRadius: 6,
+        backgroundColor: 'red',
+    },
       goalText: {
         color: 'white',
+        padding: 8,
       }
 });
